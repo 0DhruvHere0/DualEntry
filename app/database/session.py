b42f5114a8ee316f7c662ba6933f3237516ed4a4
Dhruv Gupta <dhruvgupta.26.06.2005@gmail.com>
2026-08-06 10:37:03 +0530

    DB Setup and bridge complete

--- app/database/session.py
+++ app/database/session.py
@@ -1,6 +1,9 @@
+from os import getenv
+from dotenv import load_dotenv
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-DATABASE_URL = ""
+load_dotenv()
+DATABASE_URL = getenv("DATABASE_URL")
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(
     bind=engine,
